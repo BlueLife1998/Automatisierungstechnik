74c4d08364a90da3b29b716e3e02b29fdb526e63
Luca7177 <102238320+Luca7177@users.noreply.github.com>
2022-06-08 16:50:16 +0200

    Add files via upload

---
+++ alarmsystem V2_1 08.06.22.py
@@ -0,0 +1,148 @@
+# alarmsystem.py
+# Überwachungssystem basierend auf der IMU des M5-Sticks
+# Gruppe 22
+
+import time
+import paho.mqtt.client as mqtt  # Lib of
+import RPi.GPIO as GPIO
+
+# serverAddress, below is the pi's host name. But, since the Mosquitto broker and
+# this program (which acts as the subscriber) are on the same Raspberry Pi
+# it is recommended to simply use "localhost" as the server name.
+
+serverAddress = "localhost"
+username = "user01"  # usernames and passwords are stored in the passwd file of mosquitto
+password = "MXv65bWrGGH4BKY2"
+
+clientName = "alarmActor"  # Client name can be changed here
+
+mqttClient = mqtt.Client(clientName)
+
+didPrintSubscribeMessage = False  # Flag to indicate subscribe confirmation hasn't been printed yet.
+
+alarmActive = True  # Variable that states if alarm system is active or inactive, preset alarm status active
+alarmReset = True  # Variable that states if alarm is reset, after being tripped
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)  # Broadcom numbers, don't change to BOARD
+
+buzzer = 4  # GPIO 4 for buzzer
+buttonS1 = 23  # GPIO 23 for button S1
+buttonS2 = 24  # GPIO 24 for button S2
+
+# Set the GPIO pins for the buttons as input, for the buzzer as output
+GPIO.setup(buttonS1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(buttonS2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(buzzer, GPIO.OUT)
+
+
+def connectionStatus():
+    """
+    Subscribe to topics and give out that script is successfully subscriped
+    """
+    global didPrintSubscribeMessage
+    if not didPrintSubscribeMessage:
+        didPrintSubscribeMessage = True
+        print("subscribing")
+        mqttClient.subscribe("alarmactivation")  # topic for messages to set the alarmsystem on or off
+        mqttClient.subscribe("alarmistripped")  # topic to send message to trip the alarm
+        mqttClient.subscribe("m5connected")  # topic in which M5 sticks message when they were connected
+        print("subscribed")
+
+    # Check the preset alarm status and message it to all clients
+    if alarmActive == True:
+        mqttClient.publish("alarmactivation", "alarmActivate")
+    else:
+        mqttClient.publish("alarmactivation", "alarmDeactivate")
+
+
+def messageDecoder(msg):
+    """
+    Decodes the passed message and does stuff.
+
+    @param msg: The message to be decoded.
+    """
+    message = msg.payload.decode(encoding='UTF-8')
+    topic = msg.topic  # get topic of the send message
+    global alarmActive  # set alarmActive as global, so other functions can change and use it
+    global alarmReset  # set alarmReset as global, so other functions can change and use it
+
+    if message == "alarmActivate":  # If message is "alarmActivate" the alarm gets activated
+        alarmActive = True
+        print("--- Alarm armed !!! ---")
+    elif message == "alarmDeactivate":  # If message is "alarmActivate" the alarm gets deactivated
+        alarmActive = False
+        print("--- Alarm disarmed !!! ---")
+
+    elif message == "alarmTripped":
+        if alarmActive:  # Check if alarm is activated, before it goes on
+            alarmReset = False  # Set alarm to non-resetted
+            print("--- Alarm tripped !!! ---")
+            buzzeralarm()  # call function Buzzeralarm
+
+    # If any message was sent in "m5connected" the message is shown in the terminal.
+    # It shows which m5stick was connected. Example "m5stick Kitchen was connected"
+    elif topic == 'm5connected':
+        print(message, "was connected")
+
+
+def buzzeralarm():
+    """
+    Luca schreibt hier noch nen Commi
+    """
+    # Buzzer makes noise until S2 was pressed
+    while alarmReset == False:
+        # Turn buzzer on for 1 second
+        GPIO.output(buzzer, GPIO.HIGH)
+        time.sleep(1)
+
+        # Turn buzzer off for a half second
+        GPIO.output(buzzer, GPIO.LOW)
+        time.sleep(0.5)
+
+
+def buttonS1_pressed():
+    """
+    Button S1 is able to activate and deactivate the alarm.
+    """
+    global alarmActive
+
+    # Activate alarm via button
+    if alarmActive:
+        print("Button S1 was pushed, alarm armed!")
+        mqttClient.publish("alarmactivation", "alarmActivate")  # Publish message to tell all clients alarm is active
+        alarmActive = True  # Covering the case message was not send, alarm will still be abled in the script
+    # Deactivate alarm via button
+    elif alarmActive:
+        print("Button S1 was pushed, alarm disarmed!")
+        mqttClient.publish("alarmactivation",
+                           "alarmDeactivate")  # Publish message to tell all clients alarm is inactive
+        alarmActive = False  # Covering the case message was not send, alarm will still be disabled in the script
+
+
+def buttonS2_pressed():
+    """"
+    Button S2 is able to reset the alarm after it was tipped
+    """
+    global alarmReset
+
+    if alarmReset:
+        alarmReset = True  # Reset the tripped alarm
+        print("Button S2 was pushed, alarm was resettet!")
+
+
+if __name__ == '__main__':
+
+    GPIO.add_event_detect(buttonS1, GPIO.RISING, callback=buttonS1_pressed)  # check if S1 was pressed
+    GPIO.add_event_detect(buttonS2, GPIO.RISING, callback=buttonS2_pressed)  # check if S2 was pressed
+
+    # Set up calling functions to mqttClient
+    mqttClient.on_connect = connectionStatus
+    mqttClient.on_message = messageDecoder
+
+    # Connect to the MQTT server & loop forever.
+    # CTRL-C will stop the program from running.
+    print("server address is:", serverAddress)
+    mqttClient.username_pw_set(username, password)
+    mqttClient.connect(serverAddress)
+    mqttClient.loop_forever()
